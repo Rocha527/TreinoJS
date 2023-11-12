3ac1f8c4736f8a7b93bf6ece2e847d6cbc4c45d2
Fabio <fabio_rocka@hotmail.com>
2023-11-12 15:52:55 -0300

    Fim do Curso = Introductory JavaScript

--- assets.js
+++ assets.js
@@ -1,16 +1,19 @@
   /* 
+    TREINAMENTO = EXEMPLOS DE VARIAVEIS
  INTEGER = 5,3,2,1,55,64,44444,232423423,7
  FLOAT = 0.1 , .5555, .23
  scientific notation = 111x111
+*/
 
-
- EXEMPLO E DIFERENÇA DE VARIAVEIS PARA  NUMEROS E LETRAS:
+/*
+    TREINAMENTO = EXEMPLO E DIFERENÇA DE VARIAVEIS PARA  NUMEROS E LETRAS:
  Assigning a string to a variable:
  var Strings = ("I am a string");
  var Numbers1 = 5;
+ */
 
-
- DECLARAÇÃO DE VARIAVEIS PARA INPUT NO NAVEGADOR
+/*
+    TREINAMENTO = INPUT DE NOME, IDADE, CONSOLE APRESENTA INFORMAÇÕES GERADAS
  
  var VisitorAge = prompt ("What is your age?");
 
@@ -20,7 +23,7 @@
 */
 
 /*
-DECLARAÇÃO DE INPUT PELO USUÁRIO PELO NAVEGADOR
+    TREINAMENTO = INPUT DE NOME, COM PROMPT INICIANDO A PÁGINA
 
 alert("Welcome");
 
@@ -30,8 +33,26 @@ document.write('Hello ' + VisitorName + ' Your favourite colour is ' + VisitorFa
 document.onplaying('Progress completed');
 */
 
+/* 
+    TREINAMENTO = QUANTO TEMPO VOCÊ ESTÁ VIVO? 
+
 var VisitorAge = prompt("How many years you're alive");
 var YearsAlive = parseInt(VisitorAge);
 YearsAlive = YearsAlive * 365;
 document.write("You have been alive for " + YearsAlive + " Days!" );
+*/
 
+/*   
+    TREINAMENTO = DECLARAÇÃO DE VARIÁVEL, GERANDO UM NUMERO ALEATÓRIO ENTRE *20 
+
+var RNumber = Math.floor (Math.random() *20) +1;
+alert (RNumber);
+
+    TREINAMENTO = DECLARANDO VARIÁVEL, COM INPUT DO USUÁRIO (parseInt transforma uma string para um número)
+
+var UserNumber = prompt("Pick your first Number ");
+UserNumber = parseInt(UserNumber);    
+/*
+var RNumber = Math.floor (Math.random() *UserNumber) +1;
+alert("Your random number is " + RNumber);
+*/
